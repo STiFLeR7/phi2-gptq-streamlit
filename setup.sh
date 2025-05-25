8919e8c3788c6405cc8fb2792e32fcad422675c8
STiFLeR7 <hillaniljppatel@gmail.com>
2025-05-25 14:09:46 +0530

    updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pip install -r requirements_base.txt
+
+# Upgrade pip first (optional but recommended)
+pip install --upgrade pip
+
+# Install torch and related libs first (must be before GPTQModel)
+pip install torch torchvision torchtext --extra-index-url https://download.pytorch.org/whl/cu117
+
+# Install other packages
+pip install streamlit transformers accelerate safetensors
+
+# Install GPTQModel from GitHub with --no-build-isolation to avoid build errors related to torch missing
 pip install --no-build-isolation git+https://github.com/ModelCloud/GPTQModel.git
